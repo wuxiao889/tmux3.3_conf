75731865e2b908e1ef82b01731c547d2788f6458
wxx <917660818@qq.com>
2023-05-29 17:04:21 +0800

    pane index

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 mv ~/.tmux.conf ~/.tmux.conf.bak
 
-cp ./.tmux.conf ~/
+cp ./tmux${1} ~/.tmux.conf
 
 tmux source-file ~/.tmux.conf
